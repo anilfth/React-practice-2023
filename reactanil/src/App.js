f24a8fc5cf13bfb2aadb070889a8f569e863fad1
Anil <anilfth2001@gmail.com>
2023-09-19 01:59:34 +0530

    intial commit

---
+++ reactanil/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello world in amaili fatehpur  {5 + 5}  </h1>
+      <p> awesome</p>
+      {/* <img src='' alt=''>  </img>  */}
+    </div>
+  )
+}
+
+export default App
+
