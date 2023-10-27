b1a9dc19bfb50179309147380f03bae124e2ed6b
Anil <anilfth2001@gmail.com>
2023-10-28 02:33:57 +0530

    intail commit

--- reactanil/src/Complexmultipleinputform.jsx
+++ reactanil/src/Complexmultipleinputform.jsx
@@ -4,6 +4,8 @@ function Complexmultipleinputform() {
     const[Fullname, setFullname] =  useState({
         Fname:'',
         Lname:'',
+        email: '',
+        phone: ' ',
     });
     const eventInput = (event) => {
   console.log(event.target.value);
@@ -15,20 +17,41 @@ function Complexmultipleinputform() {
   return {
     Fname: value,
     Lname:preValue.Lname,
+    email:preValue.email,
+    phone:preValue.phone,
   };
     }
     else if(name === 'Lname'){
         return{
          Fname: preValue.Fname,
          Lname:value,
+         email:preValue.email,
+         phone:preValue.phone,
+        };
+    }
+    else if(name === 'email'){
+        return{
+         Fname: preValue.Fname,
+         Lname:preValue.Lname,
+         email:value,
+         phone:preValue.phone,
+        }
+    }
+    else if(name === 'phone'){
+        return{
+         Fname: preValue.Fname,
+         Lname:preValue.Lname,
+         email:preValue.email,
+         phone:value,
         }
     }
   })
    
-  alert('Form submitted');
+  
     }
     const onSubmits = (event) =>{
         event.preventDefault();
+        // alert('Form submitted');
     }
     
   return (
@@ -36,6 +59,7 @@ function Complexmultipleinputform() {
     <div>
       <form onSubmit={onSubmits}>
    <h1> Hello,{Fullname.Fname} {Fullname.Lname} </h1>
+   <p> {Fullname.email} {Fullname.phone}</p>
    <input type='text'  name='Fname' // name attributes
    placeholder='Enter a firstName' 
    onChange={eventInput} value={Fullname.Fname}/>
@@ -45,6 +69,12 @@ function Complexmultipleinputform() {
     placeholder='Enter  lastName' onChange={eventInput} value={Fullname.Lname}/>
 
  <br/>
+ <input type='email' name='email' // name attributes
+    placeholder='Enter your email' onChange={eventInput} value={Fullname.email}/>
+ <br/>
+<input type='number' name='phone' // name attributes
+    placeholder='Enter your phone number' onChange={eventInput} value={Fullname.phone}/>
+    <br/>
  <button type='Submit'> Submit me 👍</button>
 
       </form>
